f3466bd6335afaf9fd33e8304f34c7c8a1f5b951
Paulo Borges <poolborges@hotmail.com>
2019-03-26 10:16:18 +0000

    PROJECT CREATION from start.spring.io

---
+++ src/main/kotlin/pt/isel/daws1819v/li61ng09/projectissues/Daws1819vLi61ng09ProjectissuesApplication.kt
@@ -0,0 +1,11 @@
+package pt.isel.daws1819v.li61ng09.projectissues
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Daws1819vLi61ng09ProjectissuesApplication
+
+fun main(args: Array<String>) {
+	runApplication<Daws1819vLi61ng09ProjectissuesApplication>(*args)
+}
---
+++ src/test/kotlin/pt/isel/daws1819v/li61ng09/projectissues/Daws1819vLi61ng09ProjectissuesApplicationTests.kt
@@ -0,0 +1,16 @@
+package pt.isel.daws1819v.li61ng09.projectissues
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class Daws1819vLi61ng09ProjectissuesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
