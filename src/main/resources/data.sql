e3022ff84238dd1c0f0900f0d41c05e1b3b91069
Paulo Borges <poolborges@hotmail.com>
2019-04-02 20:56:48 +0100

    Sample data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+
+
+
+/* INSERTING PROJECT DATA SAMPLE */
+insert into project(id, name, description)
+values (1, 'DAW Project', 'Projecto da disciplna DAW');
+
+
+insert into project(id, name, description)
+values (2, 'LS Project', 'Projecto da disciplna LS');
